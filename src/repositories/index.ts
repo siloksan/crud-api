909926e20770803d5c8aafd3708589d0ca8dcc42
siloksan <tutel07@gmail.com>
2024-11-18 20:38:54 +0300

    feat: add user repository

--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -1,2 +1,32 @@
-export const MyRepository = Symbol('MyRepository');
-console.log('MyRepository:');
+import { User, UsersDB } from '@/models';
+
+export class UserRepository {
+	private readonly users: UsersDB;
+	constructor(db: UsersDB) {
+		this.users = db;
+	}
+
+	public async getAll() {
+		return Array.from(this.users.values());
+	}
+
+	public async get(id: string) {
+		return this.users.get(id);
+	}
+
+	public async create(user: User) {
+		this.users.set(user.id, user);
+		return user;
+	}
+
+	public async update(id: string, user: User) {
+		this.users.set(id, user);
+		return user;
+	}
+
+	public async delete(id: string) {
+		this.users.delete(id);
+	}
+}
+
+const dontUse = 0;
