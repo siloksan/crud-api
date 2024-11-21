1f631abdc08456c1edf4f05cd13ce67e690d4d84
siloksan <tutel07@gmail.com>
2024-11-21 23:00:05 +0300

    feat: add tests for server

--- src/router/find-handler.test.ts
+++ src/router/find-handler.test.ts
@@ -1,7 +1,9 @@
 import { findHandler } from './find-handler';
 import { routes } from './routes';
-import { DYNAMIC_PATH, ERROR_MESSAGES } from '@/constants';
+import { DYNAMIC_PATH, STATUS, STATUS_MESSAGES } from '@/constants';
 
+const ERROR_METHOD = `${STATUS.BAD_REQUEST}||${STATUS_MESSAGES[STATUS.BAD_REQUEST].badRequest}`;
+const ERROR_URL = `${STATUS.NOT_FOUND}||${STATUS_MESSAGES[STATUS.NOT_FOUND]}`;
 describe('findHandler function', () => {
 	beforeEach(() => {
 		routes.children = {};
@@ -9,13 +11,13 @@ describe('findHandler function', () => {
 
 	test('should throw error when method not found', () => {
 		const method = 'POST';
-		expect(() => findHandler({ url: '/users', method })).toThrow(new Error(ERROR_MESSAGES.methodNotFound(method)));
+		expect(() => findHandler({ url: '/users', method })).toThrow(new Error(ERROR_METHOD));
 	});
 
 	test('should throw error when route not found', () => {
 		routes.children.GET = { children: {} };
 		const url = '/users';
-		expect(() => findHandler({ url, method: 'GET' })).toThrow(new Error(ERROR_MESSAGES.routeNotFound(url)));
+		expect(() => findHandler({ url, method: 'GET' })).toThrow(ERROR_URL);
 	});
 
 	test('should return handler when route found with static path', () => {
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,8 +1,18 @@
+import { DYNAMIC_PATH } from '@/constants';
 import { ControllerProps } from '@/types';
 
 export interface RouteNode {
 	children: Record<string, RouteNode>;
 	handler?: (args: ControllerProps) => Promise<void>;
 }
+
+export const ROUTES = {
+	API: {
+		USERS: {
+			ROUTE: 'api/users',
+			ID: `api/users/${DYNAMIC_PATH}`,
+		},
+	},
+};
 
 export const routes: RouteNode = { children: {} };
